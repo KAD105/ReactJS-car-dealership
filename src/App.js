6814420d9397454b41dc57666ba965b70aa9741e
KAD105 <kazem_abdi@yahoo.com>
2022-02-14 09:39:09 +0100

    Navbar updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Grids from "./components/Body/Grids";
 import Header from "./components/Header/Header";
+
 function App() {
   return (
     <div>
       <Header />
+      <Grids />
     </div>
   );
 }
---
+++ src/components/Body/Grids.js
@@ -0,0 +1,84 @@
+import "./Grids.css";
+const Grids = (props) => {
+  return (
+    <main className="grid_container">
+      <div className="grid-1 grid-col-span-2">
+        {" "}
+        <p>
+          Nam vel ornare nibh, id condimentum velit. Curabitur condimentum
+          tempor metus non euismod. Suspendisse potenti. Etiam fringilla augue
+          eu nibh egestas viverra. Mauris eu est metus. Nam nec molestie purus,
+          vel porttitor dui. Donec viverra, lacus eleifend porttitor lacinia,
+          quam tellus luctus odio, ut tincidunt mi felis in velit. Aliquam
+          condimentum maximus facilisis. Aenean finibus nisl at massa lobortis,
+          ac interdum nisl lobortis. Nunc a elit nec lorem tempor hendrerit ut
+          quis neque. Aliquam erat volutpat. Vivamus a magna massa. Sed euismod,
+          neque nec faucibus ultricies, tellus lorem posuere sapien, sit amet
+          finibus quam quam nec arcu.
+        </p>
+      </div>
+      <div className="grid-2">
+        {" "}
+        <p>
+          Nam vel ornare nibh, id condimentum velit. Curabitur condimentum
+          tempor metus non euismod. Suspendisse potenti. Etiam fringilla augue
+          eu nibh egestas viverra.Donec elementum ut odio vel cursus. Donec
+          mollis, nisl sit amet volutpat gravida, massa lectus semper leo, id
+          faucibus risus erat bibendum mi.
+        </p>
+      </div>
+      <div className="grid-3">
+        {" "}
+        <p>
+          Nam vel ornare nibh, id condimentum velit. Curabitur condimentum
+          tempor metus non euismod. Suspendisse potenti. Etiam fringilla augue
+          eu nibh egestas viverra. Mauris eu est metus. Nam nec molestie purus,
+          vel porttitor dui. Donec viverra, lacus eleifend porttitor lacinia,
+          quam tellus luctus odio,
+        </p>
+      </div>
+      <div className="grid-4 grid-col-span-2">
+        {" "}
+        <p>
+          Nam vel ornare nibh, id condimentum velit. Curabitur condimentum
+          tempor metus non euismod. Suspendisse potenti. Etiam fringilla augue
+          eu nibh egestas viverra. Mauris eu est metus. Nam nec molestie purus,
+          vel porttitor dui. Donec viverra,Nam vel ornare nibh, id condimentum
+          velit. Curabitur condimentum tempor metus non euismod. Suspendisse
+          potenti. Etiam fringilla augue eu nibh egestas viverra. Mauris eu est
+          metus. Nam nec molestie purus, vel porttitor dui. Donec viverra, lacus
+          eleifend porttitor lacinia, quam tellus luctus odio,Nam vel ornare
+          nibh, id condimentum velit. Curabitur condimentum tempor metus non
+          euismod. Suspendisse potenti. Etiam fringilla augue eu nibh egestas
+          viverra. Mauris eu est metus. Nam nec molestie purus, vel porttitor
+          dui. Donec viverra, lacus eleifend porttitor lacinia, quam tellus
+          luctus odio,
+        </p>
+      </div>
+      <div className="grid-5">
+        {" "}
+        <p>
+          <strong>last one</strong> Nam vel ornare nibh, id condimentum velit.
+          Curabitur condimentum tempor metus non euismod. Suspendisse potenti.
+          Etiam fringilla augue eu nibh egestas viverra. Mauris eu est metus.
+          Nam nec molestie purus, vel porttitor dui. Donec viverra, lacus
+          eleifend porttitor lacinia, quam tellus luctus odio, ut tincidunt mi
+          felis in velit. Aliquam condimentum maximus facilisis. Aenean finibus
+          nisl at massa lobortis, ac interdum nisl lobortis. Nunc a elit nec
+          lorem tempor hendrerit ut quis neque. Aliquam erat volutpat. Vivamus a
+          magna massa. Sed euismod, neque nec faucibus ultricies, tellus lorem
+          posuere sapien, sit amet finibus quam quam nec arcu. Donec elementum
+          ut odio vel cursus. Donec mollis, nisl sit amet volutpat gravida,
+          massa lectus semper leo, id faucibus risus erat bibendum
+          mi.Suspendisse potenti. Etiam fringilla augue eu nibh egestas
+          viverra.Donec elementum ut odio vel cursus. Donec mollis, nisl sit
+          amet volutpat gravida, massa lectus semper leo, id faucibus risus erat
+          bibendum mi.
+        </p>
+      </div>
+      {/* <div className="grid-6"> grid-6</div>
+      <div className="grid-7"> grid-7</div> */}
+    </main>
+  );
+};
+export default Grids;
