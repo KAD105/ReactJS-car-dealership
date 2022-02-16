e25c20263564dc5cc96655a81cf715750a1e59cd
KAD105 <kazem_abdi@yahoo.com>
2022-02-16 07:46:55 +0100

    Photo added

--- src/components/Popular/Popular.js
+++ src/components/Popular/Popular.js
@@ -12,51 +12,65 @@ const Popular = () => {
     <main>
       <div className="container">
         <div className="grids">
-          <card className="card">
+          <card className="card stacked">
             <img src={car_1} className="card__img" />
-            <h2 className="card__title">Pellentesque Vestibulum</h2>
-            <p className="card__price"> €70.000</p>
-            <p className="card__description">Lorem ipsum dolor sit amet</p>
+            <div className="card__content">
+              <h2 className="card__title">Pellentesque Vestibulum</h2>
+              <p className="card__price"> €70.000</p>
+              <p className="card__description">Lorem ipsum dolor sit amet</p>
+            </div>
           </card>
-          <card className="card">
+          <card className="card stacked">
             <img src={car_2} className="card__img" />
-            <h2 className="card__title">Sodales Ipsum</h2>
-            <p className="card__price">€74.300</p>
-            <p className="card__description">
-              Pellentesque vestibulum ante nec leo
-            </p>
+            <div className="card__content">
+              <h2 className="card__title">Sodales Ipsum</h2>
+              <p className="card__price">€74.300</p>
+              <p className="card__description">
+                Pellentesque vestibulum ante nec leo
+              </p>
+            </div>
           </card>
-          <card className="card">
+          <card className="card stacked">
             <img src={car_3} className="card__img" />
-            <h2 className="card__title">Euismod Lacinia</h2>
-            <p className="card__price">€133.600</p>
-            <p className="card__description">Congue, tincidunt ligula</p>
+            <div className="card__content">
+              <h2 className="card__title">Euismod Lacinia</h2>
+              <p className="card__price">€133.600</p>
+              <p className="card__description">Congue, tincidunt ligula</p>
+            </div>
           </card>
-          <card className="card">
+          <card className="card stacked">
             <img src={car_4} className="card__img" />
-            <h2 className="card__title">Vestibulum Urna</h2>
-            <p className="card__price">€88.000</p>
-            <p className="card__description">Etiam mattis tincidunt urna</p>
+            <div className="card__content">
+              <h2 className="card__title">Vestibulum Urna</h2>
+              <p className="card__price">€88.000</p>
+              <p className="card__description">Etiam mattis tincidunt urna</p>
+            </div>
           </card>
-          <card className="card">
+          <card className="card stacked">
             <img src={car_5} className="card__img" />
-            <h2 className="card__title">Semper Hendrerit.</h2>
-            <p className="card__price"></p>
-            <p className="card__description">
-              Sed consequat turpis ut urna pulvinar
-            </p>
+            <div className="card__content">
+              <h2 className="card__title">Semper Hendrerit.</h2>
+              <p className="card__price">€654.457</p>
+              <p className="card__description">
+                Sed consequat turpis ut urna pulvinar
+              </p>
+            </div>
           </card>
-          <card className="card">
+          <card className="card stacked">
             <img src={car_6} className="card__img" />
-            <h2 className="card__title">Luctus Amet Libero</h2>
-            <p className="card__price">€299.740</p>
-            <p className="card__description">Suspendisse a dictum elit,</p>
+            <div className="card__content">
+              <h2 className="card__title">Luctus Amet Libero</h2>
+              <p className="card__price">€299.740</p>
+              <p className="card__description">Suspendisse a dictum elit,</p>
+            </div>
           </card>
-          <card className="card">
+          <card className="card stacked">
             <img src={car_7} className="card__img" />
-            <h2 className="card__title">Lorem Ipsum</h2>
-            <p className="card__price">€318.086</p>
-            <p className="card__description">Tempor augue dapibus in.</p>
+            <div className="card__content">
+              <h2 className="card__title">Lorem Ipsum</h2>
+              <p className="card__price">€318.086</p>
+              <p className="card__description">Tempor augue dapibus in.</p>
+            </div>
           </card>
         </div>
       </div>
